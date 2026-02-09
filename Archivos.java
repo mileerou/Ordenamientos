6ea23322a8366bb15353f6f8306dd29708111675
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-02-09 07:24:51 -0600

    Archivos.

--- Archivos.java
+++ Archivos.java
@@ -1,5 +1,24 @@
-
+import java.io.*;
+import java.util.Random;
 
 public class Archivos {
-    
+    public void generarArchivo(String nombreArchivo, int cantidad){
+        Random random = new Random();
+        try (PrintWriter writer = new PrintWriter(new FileWriter(nombreArchivo))){
+            for (int i = 0; i < cantidad; i++){
+                writer.println(random.nextInt(10000));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Integer[] leerArchivo(String nombreArchivo) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            return reader.lines().map(Integer::parseInt).toArray(Integer[]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new Integer[0];
+        }
+    }
 }
--- Radixsort.java
+++ Radixsort.java
@@ -1,3 +1,10 @@
+// El radix sort tiene una complejidad de O(d*(n+b)) donde d es el número de dígitos, n es el número de elementos y b es la base del sistema numérico.
+
 public class Radixsort <T extends Comparable<T>> {
-    
+    public void ordenar(Integer[] arreglo) {
+        int max = obtenerMaximo(arreglo);
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            contarSort(arreglo, exp);
+        }
+    }
 }
